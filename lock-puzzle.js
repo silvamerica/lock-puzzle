e937d1cee0511837395d06f0ee4bd09fadecee53
Nicholas Silva <nick@silvamerica.com>
2014-07-19 21:11:24 -0700

    Added Levels and Completion Tracking

--- lock-puzzle.js
+++ lock-puzzle.js
@@ -1,36 +1,43 @@
 (function($, window, document, undefined){
     'use strict';
-    var blocksWide = 6,
-        blocksHigh = 6,
+    var blocksWide = 2,
+        blocksHigh = 2,
         blockSize = 40,
-        blocks = [],
+        blocks = {},
+        answerBlocks = {},
         blockClasses = ['puzzle-block-left', 'puzzle-block-top', 'puzzle-block-right', 'puzzle-block-bottom'],
         blockStrengths = ['puzzle-block-low', 'puzzle-block-med', 'puzzle-block-high'],
         puzzleBlockTemplate = '<div class="puzzle-block"><div></div></div>',
-        swapChain = [];
+        swapChain = [],
+        score = 0,
+        level = 1;
 
     function PuzzleBlock(options) {
         this.class = options.class;
         this.strength = options.strength;
-        this.isHiddenBlock = false;
         this.$el = $(template(puzzleBlockTemplate, options));
         this.$el.addClass(this.class);
         this.$el.addClass(this.strength);
         this.moveTo(options.x, options.y);
-        var me = this;
-        this.$el.on('dblclick', function() {
-            me.rotate();
-            return false;
-        }).on('click', function() {
-            swapBlocks(me);
-            return false;
-        });
+        if (!options.answer) {
+            var me = this;
+            this.$el.on('dblclick', function() {
+                swapChain = [];
+                me.rotate();
+                return false;
+            }).on('click', function() {
+                swapBlocks(me);
+                return false;
+            });
+        }
     }
 
     PuzzleBlock.prototype.rotate = function() {
         var nextClass = blockClasses[blockClasses.indexOf(this.class) + 1] || blockClasses[0];
         this.$el.toggleClass([this.class, nextClass].join(' '));
         this.class = nextClass;
+        incrementScore();
+        checkEquality();
     };
 
     PuzzleBlock.prototype.moveTo = function(x, y) {
@@ -39,6 +46,36 @@
         this.$el.css({left: x * blockSize, top: y * blockSize});
     };
 
+    function checkEquality() {
+        var location, x, y;
+        for (x = 0; x < blocksHigh; x++) {
+            for (y = 0; y < blocksWide; y++) {
+                location = x + '-' + y;
+                if (blocks[location].strength !== answerBlocks[location].strength ||
+                    blocks[location].class !== answerBlocks[location].class) {
+                        return false;
+                }
+            }
+        }
+        // If we make it this far, reset.
+        increaseDifficulty();
+    }
+
+    function increaseDifficulty() {
+        setTimeout(function() {
+            blocksHigh = blocksHigh + 1;
+            blocksWide = blocksWide + 1;
+            level = level + 1;
+            $('.level').text(level);
+            initPuzzle();
+        }, 750);
+    }
+
+    function incrementScore() {
+        score = score + 1;
+        $('.score').text(score);
+    }
+
     function swapBlocks(block) {
         if (swapChain.length === 0) {
             swapChain.push(block);
@@ -46,22 +83,22 @@
         }
         if (isAdjacent(swapChain[swapChain.length - 1], block)) {
             swapChain.push(block);
+        } else {
+            swapChain = [];
         }
         if (swapChain.length >= 2) {
-            var containsHiddenBlock = false;
-            for (var i = 0; i < swapChain.length; i++) {
-                if (swapChain[i].isHiddenBlock) {
-                    containsHiddenBlock = true;
-                }
-            }
-            if (containsHiddenBlock) {
+            if (true) {
                 var firstBlock = {x: swapChain[0].x, y: swapChain[0].y};
                 for (var i = 0, nextBlock; i < swapChain.length; i++) {
                     nextBlock = swapChain[i+1] || firstBlock;
                     swapChain[i].moveTo(nextBlock.x, nextBlock.y);
+                    // Move the blocks around in the block list so we can check for equality
+                    blocks[nextBlock.x + '-' + nextBlock.y] = swapChain[i];
                 }                
             }
             swapChain = [];
+            incrementScore();
+            checkEquality();
         }
     }
 
@@ -82,33 +119,50 @@
     }
 
     function initPuzzle() {
-        var puzzleBlock;
-        var hiddenBlockIndex = {x: Math.floor(Math.random()*blocksHigh), y: Math.floor(Math.random()*blocksWide)};
-        for (var x = 0; x < blocksHigh; x++) {
-            for (var y = 0; y < blocksWide; y++) {
+        var puzzleBlock, x, y;
+        blocks = {};
+        answerBlocks = {};
+        $('.puzzle, .puzzle-answer').empty().css({width:blockSize * blocksWide + 'px', height: blockSize * blocksHigh + 'px'});
+        $('.puzzle-container').css({width:(blockSize * blocksWide * 2) + 90 + 'px'});
+        for (x = 0; x < blocksHigh; x++) {
+            for (y = 0; y < blocksWide; y++) {
                 puzzleBlock = new PuzzleBlock({
                     x: x,
                     y: y,
                     class: blockClasses[Math.floor(Math.random()*blockClasses.length)],
                     strength: blockStrengths[Math.floor(Math.random()*blockStrengths.length)],
                 });
-                if (x === hiddenBlockIndex.x && y === hiddenBlockIndex.y) {
-                    puzzleBlock.isHiddenBlock = true;
-                    puzzleBlock.$el.addClass('hidden-block');
-                }
-                blocks.push(puzzleBlock);
+                blocks[x + '-' + y] = puzzleBlock;
                 $('.puzzle').append(puzzleBlock.$el);
             }
         }
-
-        
+        // Set up the answer blocks
+        var randomLocations = [],
+            randomLocation,
+            answerBlock;
+        for (x = 0; x < blocksHigh; x++) {
+            for (y = 0; y < blocksWide; y++) {
+                randomLocations.push(x + '-' + y);
+            }
+        }
+        for (x = 0; x < blocksHigh; x++) {
+            for (y = 0; y < blocksWide; y++) {
+                randomLocation = randomLocations.splice(Math.floor( Math.random() * randomLocations.length ), 1)[0];
+                answerBlock = new PuzzleBlock({
+                    x: x,
+                    y: y,
+                    class: blockClasses[Math.floor(Math.random()*blockClasses.length)],
+                    strength: blocks[randomLocation].strength,
+                    answer: true
+                });
+                answerBlocks[x + '-' + y] = answerBlock;
+                $('.puzzle-answer').append(answerBlock.$el);
+            }
+        }
     }
 
     $(function() {
         initPuzzle();
-        // $('.puzzle').on('click', function(event) {
-        //     handleClick(event);
-        // });
     });
 
 })(jQuery, window, document);
